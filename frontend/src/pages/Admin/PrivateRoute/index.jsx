62058fa6f241d90990f548cf586393bcfa51349c
anshrusia200 <anshrusia2002@gmail.com>
2022-11-05 00:00:00 +0530

    :sparkles: Added toast notification

--- frontend/src/pages/Admin/PrivateRoute/index.jsx
+++ frontend/src/pages/Admin/PrivateRoute/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 export const AdminPrivateRoute = ({ children }) => {
   const user = useSelector((store) => store.user);
@@ -8,6 +9,7 @@ export const AdminPrivateRoute = ({ children }) => {
   if (!user.data) {
     return <Navigate to='/admin/login' />;
   } else if (user.data.role !== 'admin') {
+    toast.error(`The role ${user.data.role} cannot access this resource`);
     return <Navigate to={`/${user.data.role}/dashboard`} />;
   } else {
     return children;
