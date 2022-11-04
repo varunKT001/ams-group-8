00f2fac3bf2cfd4547416cc9c4484bbbb5e5f67f
varunKT001 <tiwarivarun819.vt33.vt@gmail.com>
2022-11-05 00:00:00 +0530

    :sparkles: Added authentication for scanner

--- frontend/src/pages/Scanner/index.jsx
+++ frontend/src/pages/Scanner/index.jsx
@@ -4,13 +4,14 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import QRScanner from 'react-qr-scanner';
 import { toast } from 'react-toastify';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { markAttendance } from '../../api/attendance';
 import { Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 export default function Scanner() {
   const dispatch = useDispatch();
+  const user = useSelector((store) => store.user);
   const [id, setId] = useState('');
 
   function handleScan(data) {
@@ -30,6 +31,32 @@ export default function Scanner() {
     }
     return () => clearTimeout(timeout);
   }, [id]);
+
+  if (!user.data) {
+    return (
+      <Box
+        display='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'
+        minHeight='100vh'
+      >
+        <Stack spacing={3}>
+          <Typography>
+            Login as an Admin to activate the QR Code Scanner
+          </Typography>
+          <Link
+            to='/admin/login'
+            style={{ width: '100%', display: 'grid', placeItems: 'center' }}
+          >
+            <Button variant='contained' sx={{ width: 'fit-content' }}>
+              Login
+            </Button>
+          </Link>
+        </Stack>
+      </Box>
+    );
+  }
 
   return (
     <Box
