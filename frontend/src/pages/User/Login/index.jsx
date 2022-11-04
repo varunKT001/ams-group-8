753924b043621d7265dc5f396d60fc633c28f6af
samarthgarg09 <samarthgarg92001@gmail.com>
2022-11-05 00:00:00 +0530

    :sparkles: Login forms refactored

--- frontend/src/pages/User/Login/index.jsx
+++ frontend/src/pages/User/Login/index.jsx
@@ -1,85 +1,130 @@
-import React from 'react';
-import './user-login.css';
-import { TextField, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
-import useForm from '../../../components/useForm/useForm';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { useDispatch, useSelector } from 'react-redux';
+// import { login } from '../api/user';
+import { Navigate } from 'react-router-dom';
 
-const initialFValues = {
-  userId: '',
-  userPassword: '',
+function Copyright(props) {
+  return (
+    <Typography
+      variant='body2'
+      color='text.secondary'
+      align='center'
+      {...props}
+    >
+      {'Copyright © '}
+      <Link color='inherit' href='https://mui.com/'>
+        AMS
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const theme = createTheme();
+
+const initialCredentials = {
+  phoneNumber: '',
+  password: '',
 };
 
-export const UserLogin = () => {
-  const validate = (fieldValues = values) => {
-    let temp = { ...errors };
-    if ('userId' in fieldValues)
-      temp.userId = fieldValues.userId.length == 5 ? '' : 'Invalid User Id';
+export function UserLogin() {
+  // const dispatch = useDispatch();
+  // const user = useSelector((store) => store.user);
+  const [credentials, setCredentials] = React.useState(initialCredentials);
 
-    if ('userPassword' in fieldValues)
-      temp.userPassword =
-        fieldValues.userPassword.length >= 8
-          ? ''
-          : 'Minimum 8 characters required.';
-
-    setErrors({
-      ...temp,
+  const handleCredentials = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setCredentials((prev) => {
+      return { ...prev, [name]: value };
     });
-
-    if (fieldValues == values) return Object.values(temp).every((x) => x == '');
   };
 
-  const { values, setValues, errors, setErrors, handleInputChange, resetForm } =
-    useForm(initialFValues, true, validate);
+  const handleSubmit = () => {
+    // dispatch(login(credentials));
+  };
+
+  // if (user.data) {
+  //   return <Navigate to='/' />;
+  // }
+
   return (
-    <div className='main'>
-      <div className='card'>
-        <div className='card-head'>User Login</div>
-        <form action='' className='card-form'>
-          <TextField
-            label='User Id'
-            name='userId'
-            fullWidth
-            required
-            autoComplete='off'
-            margin='dense'
-            color='secondary'
-            value={values.userId}
-            onChange={handleInputChange}
-            error={errors.userId ? true : false}
-            helperText={errors.userId}
-          />
-          <TextField
-            label='Password'
-            type='password'
-            fullWidth
-            required
-            name='userPassword'
-            margin='dense'
-            color='secondary'
-            value={values.userPassword}
-            onChange={handleInputChange}
-            error={errors.userPassword ? true : false}
-            helperText={errors.userPassword}
-          />
-          <Button
-            variant='contained'
-            id=''
-            type='submit'
-            style={{
-              backgroundColor: '#D4EDDA',
-              color: '#525759',
-              margin: '0.5rem 0',
-            }}
+    <ThemeProvider theme={theme}>
+      <Container component='main' maxWidth='xs'>
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component='h1' variant='h5'>
+            User Login
+          </Typography>
+          <Box
+            component='form'
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
           >
-            <b>Login</b>
-          </Button>
-        </form>
-        <div className='change-page'>
-          <Link to='/admin-login' className='toggle-link'>
-            Admin Login →
-          </Link>
-        </div>
-      </div>
-    </div>
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              id='phoneNumber'
+              label='Phone Number'
+              name='phoneNumber'
+              autoComplete='phoneNumber'
+              autoFocus
+              onChange={handleCredentials}
+            />
+            <TextField
+              margin='normal'
+              required
+              fullWidth
+              name='password'
+              label='Password'
+              type='password'
+              id='password'
+              autoComplete='current-password'
+              onChange={handleCredentials}
+            />
+            <Button
+              fullWidth
+              variant='contained'
+              sx={{ mt: 3, mb: 2 }}
+              onClick={handleSubmit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href='/admin/login' variant='body2'>
+                  Login as an Admin
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+        <Copyright sx={{ mt: 8, mb: 4 }} />
+      </Container>
+    </ThemeProvider>
   );
-};
+}
